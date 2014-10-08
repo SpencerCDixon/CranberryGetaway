bdbe8e20020068795f700572406e3bdbd6aee9db
Spencer Dixon <spencercdixon@gmail.com>
2014-10-08 17:55:14 -0400

    Added admin authorize helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception 
+  
+  def admin_authorize!
+    if !current_user.admin?
+      flash[:warning] = "Invalid request"
+      redirect_to root_path
+    end
+  end
 end
